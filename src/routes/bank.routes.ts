15a63630f53abbb5ab5d98e75fda0eaf536c1640
Vitor Schmidt <schmidt_vitor@hotmail.com>
2022-07-12 20:31:17 -0300

    feat/Routes adding user, bank, client, debts routes structure

---
+++ src/routes/bank.routes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const bankRoutes = Router();
+
+bankRoutes.post("");
+
+export default bankRoutes;
---
+++ src/routes/debts.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const debtsRoutes = Router();
+
+debtsRoutes.post("");
+debtsRoutes.get("/:id");
+
+export default debtsRoutes;
