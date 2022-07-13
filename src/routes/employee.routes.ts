6737e41585df307a59b5fd699b40abc262744df6
Vinicius <vinicius.stmartins@gmail.com>
2022-07-13 14:00:14 -0300

    fix: adjust route employee

---
+++ src/routes/employee.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+const employeeRoutes = Router();
+
+employeeRoutes.get("");
+employeeRoutes.post("");
+// employeeRoutes.patch("/:id");
+// employeeRoutes.delete("/:id");
+// employeeRoutes.get("/info/:id");
+// employeeRoutes.post("/info/:id");
+// employeeRoutes.patch("/info/:id");
+// employeeRoutes.delete("/info/:id");
+
+export default employeeRoutes;
