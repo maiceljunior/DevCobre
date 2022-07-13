1dae440bc5797031e0eed6a029d3ebe9ef753659
Vitor Schmidt <schmidt_vitor@hotmail.com>
2022-07-13 16:36:10 -0300

    feat/Employee list one employer

--- src/routes/employee.routes.ts
+++ src/routes/employee.routes.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import createEmployeeController from "../controllers/employee/createEmployee.controller";
+import listEmployeeController from "../controllers/employee/listEmployee.controller";
 import duplicatedEmailMiddleware from "../middlewares/duplicatedEmail.middleware";
 
 const routes = Router();
 
 export const employeeRoutes = () => {
   routes.post("/create", duplicatedEmailMiddleware, createEmployeeController);
+  routes.get("/info/:id", listEmployeeController);
 };
 // routes.patch("/:id");
 // routes.delete("/:id");
-// routes.get("/info/:id");
 // routes.post("/info/:id");
 // routes.patch("/info/:id");
 // routes.delete("/info/:id");
