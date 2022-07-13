c4e603d8a6f1e1d5444c4446854f464080a30280
Vinicius <vinicius.stmartins@gmail.com>
2022-07-13 13:54:30 -0300

    fix: adjust route Client

--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -2,7 +2,13 @@ import { Router } from "express";
 
 const clientRoutes = Router();
 
+clientRoutes.get("");
 clientRoutes.post("");
-clientRoutes.get("/:id");
+// clientRoutes.patch("/:id");
+// clientRoutes.delete("/:id");
+// clientRoutes.get("/info/:id");
+// clientRoutes.post("/info/:id");
+// clientRoutes.patch("/info/:id");
+// clientRoutes.delete("/info/:id");
 
 export default clientRoutes;
