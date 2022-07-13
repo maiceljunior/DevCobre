3412acdd58ae8f141e114c1923e31dbb39eae0d4
Vinicius <vinicius.stmartins@gmail.com>
2022-07-13 13:51:46 -0300

    fix: adjust route Bank

--- src/routes/bank.routes.ts
+++ src/routes/bank.routes.ts
@@ -2,6 +2,13 @@ import { Router } from "express";
 
 const bankRoutes = Router();
 
+bankRoutes.get("");
 bankRoutes.post("");
+// bankRoutes.patch(":/id");
+// bankRoutes.delete(":/id");
+// bankRoutes.get("/contact/:id");
+// bankRoutes.post("/contact/:id");
+// bankRoutes.patch("/contact/:id");
+// bankRoutes.delete("/contact/:id");
 
 export default bankRoutes;
