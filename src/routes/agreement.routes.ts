544c4494ea5c78e02685051df3a74fb633cc5c89
Vinicius <vinicius.stmartins@gmail.com>
2022-07-13 14:14:08 -0300

    fix: create route agreement

---
+++ src/routes/agreement.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+const agreementRoutes = Router();
+
+agreementRoutes.get("");
+agreementRoutes.post("");
+// agreementRoutes.patch("/:id");
+// agreementRoutes.delete("/:id");
+// agreementRoutes.get("/status/:id");
+// agreementRoutes.post("/status/:id");
+// agreementRoutes.patch("/status/:id");
+// agreementRoutes.delete("/status/:id");
+
+
+
+export default agreementRoutes;
