347f32f28b13b90cd208d2672d498c0bc7adc186
Vinicius <vinicius.stmartins@gmail.com>
2022-07-13 14:03:04 -0300

    fix: adjust route history

---
+++ src/routes/history.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+const historyRoutes = Router();
+
+historyRoutes.get("");
+historyRoutes.post("");
+// historyRoutes.patch("/:id");
+// historyRoutes.delete("/:id");
+// historyRoutes.get("/employee/:id");
+// historyRoutes.post("/employee/:id");
+// historyRoutes.patch("/employee/:id");
+// historyRoutes.delete("/employee/:id");
+
+export default historyRoutes;
