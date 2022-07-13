a86cce2a22c4e1b3ede68c1aa2bf3719f5048e21
Vinicius <vinicius.stmartins@gmail.com>
2022-07-13 13:57:52 -0300

    fix: adjust route debts

--- src/routes/bank.routes.ts
+++ src/routes/bank.routes.ts
@@ -4,8 +4,8 @@ const bankRoutes = Router();
 
 bankRoutes.get("");
 bankRoutes.post("");
-// bankRoutes.patch(":/id");
-// bankRoutes.delete(":/id");
+// bankRoutes.patch("/:id");
+// bankRoutes.delete("/:id");
 // bankRoutes.get("/contact/:id");
 // bankRoutes.post("/contact/:id");
 // bankRoutes.patch("/contact/:id");
--- src/routes/debts.routes.ts
+++ src/routes/debts.routes.ts
@@ -2,7 +2,13 @@ import { Router } from "express";
 
 const debtsRoutes = Router();
 
+debtsRoutes.get("");
 debtsRoutes.post("");
-debtsRoutes.get("/:id");
+// debtsRoutes.patch("/:id");
+// debtsRoutes.delete("/:id");
+// debtsRoutes.get("/type/:id");
+// debtsRoutes.post("/type/:id");
+// debtsRoutes.patch("/type/:id");
+// debtsRoutes.delete("/type/:id");
 
 export default debtsRoutes;
