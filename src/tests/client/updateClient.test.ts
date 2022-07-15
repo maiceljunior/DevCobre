9e78ce808a4cbdaaaabb9870af270f81d4b01f56
Letícia Leal Moreira <lealmleticia@gmail.com>
2022-07-15 15:25:49 -0300

    ADD: Inclusion of update test and editing of client test names

---
+++ src/tests/client/updateClient.test.ts
@@ -0,0 +1,69 @@
+import { AppDataSource } from "../../data-source";
+import { DataSource } from "typeorm";
+import request from "supertest";
+import app from "../../app";
+// import test from "node:test";
+// import describe from "node:test"
+// import beforeAll from "node:test"
+// import afterAll from "node:test"
+// import expect from "node:test"
+
+describe("Testing PATCH method in /client/<document>", () => {
+  let connection: DataSource;
+
+  interface UpdateClient {
+    document: number;
+    name: string;
+    type: string;
+  }
+
+  let testClient1: UpdateClient = {
+    document: 12345678910,
+    name: "Client Test",
+    type: "Físico",
+  };
+
+  let response: any;
+
+  beforeAll(async () => {
+    await AppDataSource.initialize()
+      .then((res) => (connection = res))
+      .catch((err) => {
+        console.error("Error during Data Source initialization", err);
+      });
+
+    response = await request(app).post("/client").send(testClient1);
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  test("Trying to update a client", async () => {
+    const responsePatch = await request(app)
+      .patch(`/client/${response.body.document}`)
+      .send(testClient1.name);
+
+    const responseGet = await request(app).get(
+      `/client/${response.body.document}`
+    );
+
+    expect(responsePatch.status).toEqual(200);
+    expect(responsePatch.body).toHaveProperty("message");
+
+    expect(responseGet.body).toEqual(
+      expect.objectContaining({
+        document: response.body.document,
+        name: testClient1.name,
+        type: response.body.type,
+      })
+    );
+  });
+
+  test("Trying to update a client that doesn't exist", async () => {
+    const response = await request(app).get(`/client/1`);
+
+    expect(response.status).toEqual(404);
+    expect(response.body).toHaveProperty("message");
+  });
+});
