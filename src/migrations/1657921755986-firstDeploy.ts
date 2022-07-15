1fe68fce45f85819f1440525cd9718fa0dfd00c4
Maicel Junior <junioraraujc@gmail.com>
2022-07-15 18:49:42 -0300

    fix: test deploy

---
+++ src/migrations/1657921755986-firstDeploy.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class firstDeploy1657921755986 implements MigrationInterface {
+    name = 'firstDeploy1657921755986'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "bank" DROP CONSTRAINT "UQ_11f196da2e68cef1c7e84b4fe94"`);
+        await queryRunner.query(`ALTER TABLE "bank" DROP COLUMN "name"`);
+        await queryRunner.query(`ALTER TABLE "bank" ADD "name" character varying(250) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "bank" ADD CONSTRAINT "UQ_11f196da2e68cef1c7e84b4fe94" UNIQUE ("name")`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "bank" DROP CONSTRAINT "UQ_11f196da2e68cef1c7e84b4fe94"`);
+        await queryRunner.query(`ALTER TABLE "bank" DROP COLUMN "name"`);
+        await queryRunner.query(`ALTER TABLE "bank" ADD "name" character varying(251) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "bank" ADD CONSTRAINT "UQ_11f196da2e68cef1c7e84b4fe94" UNIQUE ("name")`);
+    }
+
+}
