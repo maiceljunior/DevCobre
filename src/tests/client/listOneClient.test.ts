36501c2a09e1473f7d21d53ae13d789fd1cda546
Letícia Leal Moreira <lealmleticia@gmail.com>
2022-07-15 21:20:20 -0300

    add: Tests CRUD client

---
+++ src/tests/client/listOneClient.test.ts
@@ -0,0 +1,52 @@
+import { AppDataSource } from "../../data-source";
+import { DataSource } from "typeorm";
+import request from "supertest";
+import app from "../../app";
+
+describe("Testing GET method in /client/<document>", () => {
+  let connection: DataSource;
+
+  interface Client {
+    document: number;
+    name: string;
+    type: string;
+  }
+
+  let testClient1: Client = {
+    document: 12345678901,
+    name: "Client Test 1",
+    type: "Pessoa Fisica",
+  };
+
+  let response: any;
+
+  beforeAll(async () => {
+    await AppDataSource.initialize()
+      .then((res) => (connection = res))
+      .catch((err) => {
+        console.error("Error during Data Source initialization", err);
+      });
+
+    response = await request(app).post("/client").send(testClient1);
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  test("Trying to list a specific client", async () => {
+    const responseGet = await request(app).get(
+      `/client/${response.body.document}`
+    );
+
+    expect(responseGet.status).toEqual(200);
+    // expect(responseGet.body.length).toEqual(1);
+    expect(responseGet.body).toEqual(
+      expect.objectContaining({
+        document: response.body.document,
+        name: response.body.name,
+        type: response.body.type,
+      })
+    );
+  });
+});
