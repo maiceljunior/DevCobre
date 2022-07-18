4e2d2dddc365f8cf4087513b5027b24c971828a6
Letícia Leal Moreira <lealmleticia@gmail.com>
2022-07-18 20:03:16 -0300

    fix: agreements entities delete

--- src/services/agreement/createAgreement.service.ts
+++ src/services/agreement/createAgreement.service.ts
@@ -4,27 +4,35 @@ import { Agreement } from "../../entities/agreement.entity";
 import { Bank } from "../../entities/bank.entity";
 import { Debts } from "../../entities/debt.entity";
 import { Client } from "../../entities/client.entity";
-import { Employee } from "../../entities/employee.entity";
+// import { Employee } from "../../entities/employee.entity";
 import { FormOfPayment } from "../../entities/formOfPayment.entity";
-import { IAgreementRequest } from "../../interfaces/agreement";
-import { IAgreementReturn } from "../../interfaces/agreement";
+import { User } from "../../entities/user.entity";
 
-const createAgreementService = async ({ agreedValue, dateAgree, status, 
-    debts, bank, client, user, formOfPayment }: any): Promise<any> => {
-        
+const createAgreementService = async ({
+  agreedValue,
+  dateAgree,
+  status,
+  debts,
+  bank,
+  client,
+  user,
+  formOfPayment,
+}: any): Promise<any> => {
   const agreementRepository = AppDataSource.getRepository(Agreement);
   const debtsRepository = AppDataSource.getRepository(Debts);
   const paymentRepository = AppDataSource.getRepository(FormOfPayment);
-  const userRepository = AppDataSource.getRepository(Employee);
+  const userRepository = AppDataSource.getRepository(User);
   const clientRepository = AppDataSource.getRepository(Client);
   const bankRepository = AppDataSource.getRepository(Bank);
-  
+
   const debtExists = await debtsRepository.findOneBy({ id: debts });
   if (!debtExists) {
     throw new AppError(409, "Debt not found!");
   }
 
-  const paymentExists = await paymentRepository.findOneBy({ id: formOfPayment });
+  const paymentExists = await paymentRepository.findOneBy({
+    id: formOfPayment,
+  });
   if (!paymentExists) {
     throw new AppError(409, "Form of payment not found!");
   }
@@ -34,7 +42,7 @@ const createAgreementService = async ({ agreedValue, dateAgree, status,
     throw new AppError(409, "Bank not found!");
   }
 
-  const clientExists = await clientRepository.findOneBy({ document: parseInt(client) });
+  const clientExists = await clientRepository.findOneBy({ document: client });
   if (!clientExists) {
     throw new AppError(409, "Client not found!");
   }
@@ -52,7 +60,7 @@ const createAgreementService = async ({ agreedValue, dateAgree, status,
     bank: bank,
     client: client,
     user: user,
-    formofpayment: formOfPayment
+    formofpayment: formOfPayment,
   });
   await agreementRepository.save(entry);
 
