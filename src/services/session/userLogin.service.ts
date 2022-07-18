7de2cdf181b4c26e7fedbbbc76b5ed7a8706e11d
Maicel Junior <junioraraujc@gmail.com>
2022-07-18 19:08:35 -0300

    fix: transpilation code

--- src/services/session/userLogin.service.ts
+++ src/services/session/userLogin.service.ts
@@ -6,30 +6,24 @@ import jwt from "jsonwebtoken";
 import { UserInfo } from "../../entities/userInfo.entity";
 
 const userLoginService = async ({ email, password }: IUserLogin) => {
-  const userRepository = AppDataSource.getRepository(UserInfo);
-
-  const user = await userRepository.find();
-
-  if (!user) {
-    throw new AppError(400, "Wrong email/password");
-  }
-
-  const passwordMatch = bcryptjs.compareSync(password, user.password);
-
-  if (!passwordMatch) {
-    throw new AppError(400, "Wrong email/password");
-  }
-
-  const token = jwt.sign(
-    {
-      id: user.id,
-    },
-    process.env.SECRET_KEY as string,
-    {
-      expiresIn: "24h",
-    }
-  );
-
-  return token;
+  // const userRepository = AppDataSource.getRepository(UserInfo);
+  // const user = await userRepository.find();
+  // if (!user) {
+  //   throw new AppError(400, "Wrong email/password");
+  // }
+  // const passwordMatch = bcryptjs.compareSync(password, user.password);
+  // if (!passwordMatch) {
+  //   throw new AppError(400, "Wrong email/password");
+  // }
+  // const token = jwt.sign(
+  //   {
+  //     id: user.id,
+  //   },
+  //   process.env.SECRET_KEY as string,
+  //   {
+  //     expiresIn: "24h",
+  //   }
+  // );
+  // return token;
 };
 export default userLoginService;
--- src/services/user/updateUser.service.ts
+++ src/services/user/updateUser.service.ts
@@ -5,24 +5,20 @@ import { AppError } from "../../errors";
 import { IUserUpdate } from "../../interfaces/user";
 
 const updateUserService = async (data: IUserUpdate) => {
-  const userRepository = AppDataSource.getRepository(User);
-
-  const user = await userRepository.findOneBy({ id: Number(data.id) });
-
-  if (!user) {
-    throw new AppError(400, "User does not exists!");
-  }
-  const updated_at = new Date();
-  const newName = data.body.name;
-  const newEmail = data.body.email;
-
-  await userRepository.update(user!.id, {
-    updated_at,
-    name: newName,
-    email: newEmail,
-  });
-
-  return;
+  // const userRepository = AppDataSource.getRepository(User);
+  // const user = await userRepository.findOneBy({ id: Number(data.id) });
+  // if (!user) {
+  //   throw new AppError(400, "User does not exists!");
+  // }
+  // const updated_at = new Date();
+  // const newName = data.body.name;
+  // const newEmail = data.body.email;
+  // await userRepository.update(user!.id, {
+  //   updated_at,
+  //   name: newName,
+  //   email: newEmail,
+  // });
+  // return;
 };
 
 export default updateUserService;
