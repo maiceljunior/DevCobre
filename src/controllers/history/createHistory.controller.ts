84bc8c419f4478147daa052376ce6bf06b26226a
Maicel Junior <junioraraujc@gmail.com>
2022-07-19 15:01:01 -0300

    fix: history relation

--- src/controllers/history/createHistory.controller.ts
+++ src/controllers/history/createHistory.controller.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from "express";
 import createContactHistoryService from "../../services/history/createHistory.service";
 const createHistoryController = async (req: Request, res: Response) => {
-    const { debts_id,date_contact, agreement,note,user_id } = req.body;
-    const newHistory = await createContactHistoryService({ debts_id,date_contact,agreement,note,user_id});
-    return res.status(201).json(newHistory);
-  };
-  
-  export default createHistoryController;
+  const { debtId, date_contact, agreement, note, userId } = req.body;
+
+  const newHistory = await createContactHistoryService({
+    debtId,
+    date_contact,
+    agreement,
+    note,
+    userId,
+  });
+
+  return res.status(201).json(newHistory);
+};
+
+export default createHistoryController;
