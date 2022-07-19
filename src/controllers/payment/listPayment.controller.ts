a738fa12d276e9c7944fea17b5ccc41c93dc447a
Letícia Leal Moreira <lealmleticia@gmail.com>
2022-07-19 13:41:00 -0300

    feaure: insertion list One Payment

--- src/controllers/payment/listPayment.controller.ts
+++ src/controllers/payment/listPayment.controller.ts
@@ -2,9 +2,9 @@ import { Request, Response } from "express";
 import listPaymentService from "../../services/payment/listPayment.service";
 
 const listPaymentController = async (req: Request, res: Response) => {
-    const payments = await listPaymentService();
-  
-    return res.json(payments);
-  };
-  
-  export default listPaymentController;
+  const payments = await listPaymentService();
+
+  return res.json(payments);
+};
+
+export default listPaymentController;
