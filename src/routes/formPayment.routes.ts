962d3114ae9d9d8bcfe3ef255b2fdba48e3aec82
Maicel Junior <junioraraujc@gmail.com>
2022-07-19 15:38:35 -0300

    add: schema in payment

--- src/routes/formPayment.routes.ts
+++ src/routes/formPayment.routes.ts
@@ -4,12 +4,14 @@ import updatePaymentController from "../controllers/payment/updatePayment.contro
 import listPaymentController from "../controllers/payment/listPayment.controller";
 import deletePaymentController from "../controllers/payment/deletePayment.controller";
 import listOnePaymentController from "../controllers/payment/listOnePayment.controller";
+import schemaValidation from "../middlewares/schemaValidation";
+import paymentSchema from "../schemas/payment/payment.schema";
 
 const routes = Router();
 
 export const formPaymentRoutes = () => {
   routes.get("", listPaymentController);
-  routes.post("", createPaymentController);
+  routes.post("", schemaValidation(paymentSchema), createPaymentController);
   routes.get("/:id", listOnePaymentController);
   routes.patch("/:id", updatePaymentController);
   routes.delete("/:id", deletePaymentController);
--- src/services/userDebt/deleteUserDebt.service.ts
+++ src/services/userDebt/deleteUserDebt.service.ts
@@ -1,19 +1,14 @@
 import { AppDataSource } from "../../data-source";
-import { UserDebt } from "../../entities/userDebt.entity";
 import { AppError } from "../../errors";
 
 const deleteUserDebtService = async (id: string) => {
-  const userDebtRepository = AppDataSource.getRepository(UserDebt);
-
-  const userDebt = await userDebtRepository.findOneBy({ id: parseInt(id) });
-
-  if (!userDebt) {
-    throw new AppError(404, "UserDebt not found!");
-  }
-
-  await userDebtRepository.delete({ id: parseInt(id) });
-
-  return true;
+  // const userDebtRepository = AppDataSource.getRepository(UserDebt);
+  // const userDebt = await userDebtRepository.findOneBy({ id: parseInt(id) });
+  // if (!userDebt) {
+  //   throw new AppError(404, "UserDebt not found!");
+  // }
+  // await userDebtRepository.delete({ id: parseInt(id) });
+  // return true;
 };
 
 export default deleteUserDebtService;
