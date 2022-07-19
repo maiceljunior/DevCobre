8d5b53660d4accf4b784ee0d493239dd0055b23b
bosilveira <bosilveira@gmail.com>
2022-07-19 16:58:47 -0300

    Feat: Agreement Schema

--- src/services/userDebt/deleteUserDebt.service.ts
+++ src/services/userDebt/deleteUserDebt.service.ts
@@ -1,17 +1,7 @@
 import { AppDataSource } from "../../data-source";
-import { UserDebt } from "../../entities/userDebt.entity";
 import { AppError } from "../../errors";
 
 const deleteUserDebtService = async (id: string) => {
-  const userDebtRepository = AppDataSource.getRepository(UserDebt);
-
-  const userDebt = await userDebtRepository.findOneBy({ id: parseInt(id) });
-
-  if (!userDebt) {
-    throw new AppError(404, "UserDebt not found!");
-  }
-
-  await userDebtRepository.delete({ id: parseInt(id) });
 
   return true;
 };
